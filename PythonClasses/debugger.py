b04946f3c979c54c7275774622c16266f7dcb6d1
EROL-er <erolzirav@gmail.com>
2025-09-13 10:56:50 -0400

    conditional statements

---
+++ PythonClasses/debugger.py
@@ -0,0 +1,11 @@
+def print_LearnKarts():
+    LearnKarts_list=[]
+    LearnKartS= "12345678910"
+
+    for letter in LearnKartS:
+        LearnKarts_list.append(letter)
+        # This statment runs each time loop runs
+        print( LearnKarts_list)
+# This statment runs each time loop ends
+print_LearnKarts()
+
