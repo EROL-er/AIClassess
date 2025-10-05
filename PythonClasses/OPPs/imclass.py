be0564324a515dde7464f229f496e8c04fa977d7
EROL-er <erolzirav@gmail.com>
2025-10-04 21:02:21 -0400

    convert to class

---
+++ PythonClasses/OPPs/imclass.py
@@ -0,0 +1,10 @@
+from impt1 import findDublicate,findSmall
+
+l = [1, 2, 3, 4, 4, 6, 6]
+print(findDublicate(l))
+
+
+
+
+l = [1, 2, 3, 4, 4, 6, 6,-5,-6]
+print(findSmall(l))
--- PythonClasses/OPPs/impt1.py
+++ PythonClasses/OPPs/impt1.py
@@ -1,5 +1,26 @@
 def allValues(**kwargs):
   for key, value in kwargs.items():
     print(key, "=", value)
+
+
+
+def findSmall(list2):
+  for i in range(len(list2)-1):
+
+    for j in range(len(list2)-1):
+     if list2[j] > list2[j + 1]:
+            temp = list2[j]
+            list2[j] = list2[j + 1]
+            list2[j + 1] = temp
+  print(list2)
+
+
   
 
+def findDublicate(l):
+    dupes = set()
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[j] == l[i]:
+                dupes.add(l[j])
+    return dupes
